f7e04c6e22c99d380fe7536796486e67528f5b9e
ludopif <ludovic.piffaretti@orange.com>
2016-02-18 13:49:51 +0100

    Update run-httpd.sh

--- run-httpd.sh
+++ run-httpd.sh
@@ -3,9 +3,9 @@
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
-rm -rf /run/httpd/*
+#rm -rf /run/httpd/*
 
 sleep 60
 
 #exec /usr/sbin/apachectl -D FOREGROUND
-exec /bin/bash
+exec /bin/ls
